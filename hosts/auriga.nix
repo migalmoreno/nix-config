74c01e0279d3171e19a2b308a11af62b7f8f10db
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-07-21 09:43:56 +0200

    auriga: Enable uefi bootloader and tweak file systems

--- hosts/auriga.nix
+++ hosts/auriga.nix
@@ -24,15 +24,33 @@ nixpkgs.lib.nixosSystem {
         kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
         initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
         loader = {
-          grub.enable = false;
-          generic-extlinux-compatible.enable = true;
+          systemd-boot.enable = false;
+          generic-extlinux-compatible.enable = false;
+          efi.canTouchEfiVariables = true;
+          grub = {
+            enable = true;
+            efiInstallAsRemovable = true;
+            efiSupport = true;
+            device = "nodev";
+          };
         };
       };
-      fileSystems."/" = {
-        device = "/dev/disk/by-label/NIXOS_SD";
-        fsType = "ext4";
-        options = [ "noatime" ];
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-label/root";
+          fsType = "ext4";
+          options = [ "noatime" ];
+        };
+        "/boot" = {
+          device = "/dev/disk/by-label/boot";
+          fsType = "vfat";
+        };
       };
+      swapDevices = [
+        {
+          device = "/dev/disk/by-label/swap";
+        };
+      ];
       time.timeZone = "Europe/Madrid";
       environment.systemPackages = with pkgs; [
         emacs
