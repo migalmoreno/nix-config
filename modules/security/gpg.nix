fc8e0e3fb229742aef72431dc7feca2ac5958edd
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-08-28 05:22:28 +0200

    security: Add gpg and password store modules

--- modules/security/gpg.nix
+++ modules/security/gpg.nix
@@ -6,10 +6,13 @@
       enable = true;
       defaultCacheTtl = 1800;
       enableSshSupport = true;
+      pinentryPackage = pkgs.pinentry-bemenu;
+      sshKeys = [ "D6B4894600BB392AB2AEDE499CBBCF3E0620B7F6" ];
     };
     programs = {
       gpg = {
         enable = true;
+        homedir = "${config.home-manager.users.${config.user}.xdg.dataHome}/gnupg";
       };
     };
   };
