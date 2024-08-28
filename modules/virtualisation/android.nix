3f0b5304b69fe847b570760ed26eb0f3a77db125
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-08-28 05:28:08 +0200

    virtualisation: Add Android

---
+++ modules/virtualisation/android.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.adb.enable = true;
+  users.users.${config.user}.extraGroups = [ "adbusers" ];
+  services.udev.packages = [ pkgs.android-udev-rules ];
+  virtualisation.waydroid.enable = true;
+  environment.systemPackages = [ pkgs.wl-clipboard ];
+  home-manager.users.${config.user} = {
+    home.packages = with pkgs; [
+      android-tools
+      payload-dumper-go
+      fdroidcl
+    ];
+  };
+}
