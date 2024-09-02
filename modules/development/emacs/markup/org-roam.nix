4f29dde8cab4591ea595556b1256cdf8124508ae
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-09-02 19:52:10 +0200

    emacs: org-roam: Add org-roam todo config

--- modules/development/emacs/markup/org-roam.nix
+++ modules/development/emacs/markup/org-roam.nix
@@ -14,11 +14,7 @@
 (setq org-roam-completion-everywhere t)
 (setq org-roam-directory "~/notes")
 (autoload 'org-roam-db-autosync-enable "org-roam")
-(with-eval-after-load 'org-roam
-  (setq org-roam-db-location
-        (concat (or (getenv "XDG_CACHE_HOME") "~/.cache")
-                "/emacs/org-roam.db"))
-  (org-roam-db-autosync-enable))
+
 (let ((map mode-specific-map))
   (define-key map (kbd "n n") 'org-roam-buffer-toggle)
   (define-key map (kbd "n f") 'org-roam-node-find)
@@ -26,7 +22,37 @@
   (define-key map (kbd "n r") 'org-roam-ref-find)
   (define-key map (kbd "n C") 'org-roam-capture))
 
-(defun rde-org-roam-open-ref ()
+(with-eval-after-load 'org-roam
+  (setq org-roam-db-location
+        (concat (or (getenv "XDG_CACHE_HOME") "~/.cache")
+                "/emacs/org-roam.db"))
+  (org-roam-db-autosync-enable)
+  (cl-defmethod org-roam-node-type ((node org-roam-node))
+    "Return the TYPE of NODE, where the TYPE is a directory of
+the node, relative to `org-roam-directory'."
+    (condition-case
+        nil
+        (file-name-nondirectory
+         (directory-file-name
+          (file-name-directory
+           (file-relative-name (org-roam-node-file node)
+                               org-roam-directory))))
+      (error "")))
+  (setq org-roam-node-display-template
+        (concat "''${type:15} ''${title:80} " (propertize "''${tags:20}" 'face 'org-tag)))
+  (setq org-roam-node-annotation-function
+        (lambda (node) (marginalia--time (org-roam-node-file-mtime node))))
+  (setq org-roam-capture-templates
+        `(("w" "work" plain "%?"
+          :if-new (file+head "work/%<%Y%m%d%H%M%S>-''${slug}.org"
+                             "#+title: ''${title}\n#+filetags: :''${Topic}:\n")
+          :unnarrowed t)
+         ("p" "personal" plain "%?"
+          :if-new (file+head "personal/%<%Y%m%d%H%M%S>-''${slug}.org"
+                             "#+title: ''${title}\n#+filetags: :''${Topic}:\n")
+          :unnarrowed t))))
+
+(defun nrde-org-roam-open-ref ()
   "Prompt you for a list of all ROAM_REFS in the current buffer."
   (interactive)
   (when (derived-mode-p 'org-mode)
@@ -61,18 +87,94 @@
     (define-key map (kbd "C-c r T") 'org-roam-tag-remove)
     (define-key map (kbd "C-c r a") 'org-roam-alias-add)
     (define-key map (kbd "C-c r A") 'org-roam-alias-remove)
-    (define-key map (kbd "C-c r O") 'rde-org-roam-open-ref)))
+    (define-key map (kbd "C-c r O") 'nrde-org-roam-open-ref)))
 
 (with-eval-after-load 'window
   (add-to-list 'display-buffer-alist
                `(,(rx "*org-roam*")
                  display-buffer-same-window)))
 
-(autoload  'org-roam-dailies-map "org-roam-dailies" "" nil 'keymap)
+(autoload 'org-roam-dailies-map "org-roam-dailies" "" nil 'keymap)
 (define-key mode-specific-map (kbd "d") 'org-roam-dailies-map)
 (with-eval-after-load 'org-roam-dailies
-  (setq org-roam-dailies-directory "daily/"))
+  (setq org-roam-dailies-directory "./")
+  (setq org-roam-dailies-capture-templates
+        '(("w" "work" entry
+           "* %?"
+           :if-new (file+head "work/daily/%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n"))
+          ("p" "personal" entry
+           "* %?"
+           :if-new (file+head "personal/daily/%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n")))))
+
+(defun nrde-org-roam-get-filetags ()
+  "Return the top-level tags for the current org-roam node."
+  (split-string
+   (or (cadr (assoc "FILETAGS"
+                    (org-collect-keywords '("filetags"))))
+       "")
+   ":" 'omit-nulls))
+
+(defun nrde-org-roam-todo-p ()
+  "Return non-nil if the current buffer has any to-do entry."
+  (org-element-map
+      (org-element-parse-buffer 'headline)
+      'headline
+    (lambda (h)
+      (eq (org-element-property :todo-type h) 'todo))
+    nil 'first-match))
+
+(defun nrde-org-roam-update-todo-tag ()
+  "Update the \"todo\" tag in the current buffer."
+  (when (and (not (active-minibuffer-window))
+             (org-roam-file-p))
+    (org-with-point-at 1
+      (let* ((tags (nrde-org-roam-get-filetags))
+             (is-todo (nrde-org-roam-todo-p)))
+        (cond ((and is-todo (not (member "todo" tags)))
+               (org-roam-tag-add '("todo")))
+              ((and (not is-todo) (member "todo" tags))
+               (org-roam-tag-remove '("todo"))))))))
+
+(defun nrde-org-roam-list-todo-files ()
+  "Return a list of org-roam files containing the \"todo\" tag."
+  (org-roam-db-sync)
+  (let ((todo-nodes (cl-remove-if-not
+                     (lambda (n)
+                       (member "todo" (org-roam-node-tags n)))
+                     (org-roam-node-list))))
+    (delete-dups (mapcar 'org-roam-node-file todo-nodes))))
+
+(defun nrde-org-roam-update-todo-files (&rest _)
+  "Update the value of `org-agenda-files'."
+  (setq org-agenda-files (nrde-org-roam-list-todo-files)))
+
+(defun nrde-org-roam-ref-add (ref node)
+  "Add REF to NODE.
+If NODE doesn't exist, create a new org-roam node with REF."
+  (interactive
+   (list
+    (read-string "Ref: ")
+    (org-roam-node-read)))
+  (if-let ((file (org-roam-node-file node)))
+      (with-current-buffer (or (find-buffer-visiting file)
+                               (find-file-noselect file))
+        (org-roam-property-add "ROAM_REFS" ref)
+        (save-buffer)
+        (kill-current-buffer))
+    (org-roam-capture-
+     :keys "r"
+     :node node
+     :info `(:ref ,ref)
+     :templates org-roam-capture-templates
+     :props '(:finalize find-file))))
+
+(with-eval-after-load 'org-roam
+  (add-hook 'org-roam-find-file-hook 'nrde-org-roam-update-todo-tag)
+  (add-hook 'before-save-hook 'nrde-org-roam-update-todo-tag))
+(advice-add 'org-agenda :before 'nrde-org-roam-update-todo-files)
 '';
-    };
-  };
+};
+};
 }
