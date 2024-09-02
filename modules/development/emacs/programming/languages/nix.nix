625bea5539a9f8cae25543fcaff01df6f0428f69
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-09-02 19:52:49 +0200

    emacs: nix: Add nix language server and nixfmt

--- modules/development/emacs/programming/languages/nix.nix
+++ modules/development/emacs/programming/languages/nix.nix
@@ -1,9 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home-manager.users.${config.user} = {
+    home.packages = with pkgs; [
+      nil
+      nixfmt-rfc-style
+    ];
     programs.emacs = {
-      extraPackages = epkgs: with epkgs; [ nix-mode ];
+      extraPackages = epkgs: [ epkgs.nix-mode ];
+      extraConfig = ''
+(add-hook 'nix-mode-hook #'eglot-ensure)
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '(nix-mode . ("nil"))))
+'';
     };
   };
 }
