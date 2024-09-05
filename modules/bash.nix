d4f7035561d64269819f0615ca1d9a0ae4367b5d
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-09-05 13:49:18 +0200

    bash: Remove unneeded Emacs mkIf

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,20 +1,13 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, pkgs, ... }:
 
 {
   home-manager.users.${config.user} = {
     programs.bash = {
       enable = true;
     };
-    programs.emacs.extraConfig =
-      lib.mkIf config.home-manager.users.${config.user}.programs.emacs.enable
-        ''
-          (with-eval-after-load 'shell
-            (setq explicit-shell-file-name "${pkgs.bashInteractive}/bin/bash"))
-        '';
+    programs.emacs.extraConfig = ''
+      (with-eval-after-load 'shell
+        (setq explicit-shell-file-name "${pkgs.bashInteractive}/bin/bash"))
+    '';
   };
 }
