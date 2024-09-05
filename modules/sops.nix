5db36d4e11bf2108b97c72c0b6e5de500be72f38
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-09-05 13:55:00 +0200

    sops: Remove general ssh key paths setting

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,9 +1,6 @@
-{ config, ... }:
-
 {
   sops = {
     defaultSopsFile = ../secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.sshKeyPaths = [ "/home/${config.user}/.ssh/id_ed25519" ];
   };
 }
