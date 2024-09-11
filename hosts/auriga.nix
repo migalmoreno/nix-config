98a823a7ce5269ffb0e1f16b59e23f1d6b93e468
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-09-11 12:26:22 +0200

    auriga: Add version control services

--- hosts/auriga.nix
+++ hosts/auriga.nix
@@ -118,6 +118,63 @@ nixpkgs.lib.nixosSystem {
             };
           };
         };
+        services.gitolite = {
+          enable = true;
+          user = "git";
+          group = "git";
+          adminPubkey = config.secrets.personal.publicSshKey;
+          extraGitoliteRc = ''
+            $RC{UMASK} = 0027;
+            $RC{GIT_CONFIG_KEYS} = "gitweb\..*";
+          '';
+        };
+        services.cgit."git.migalmoreno.com" = {
+          enable = true;
+          scanPath = "/var/lib/gitolite/repositories";
+          settings = {
+            project-list = "/var/lib/gitolite/projects.list";
+            section-from-path = true;
+            repository-directory = "/var/lib/gitolite/repositories";
+            repository-sort = "name";
+            case-sensitive-sort = false;
+            root-desc = "Miguel Ángel Moreno's Git repositories";
+            enable-git-config = true;
+            enable-index-links = false;
+            enable-index-owner = false;
+            enable-commit-graph = true;
+            enable-log-filecount = true;
+            enable-log-linecount = true;
+            readme = ":README";
+            remove-suffix = true;
+            clone-url = "https://git.migalmoreno.com/$CGIT_REPO_URL";
+            about-filter = "${
+              config.services.cgit."git.migalmoreno.com".package
+            }/lib/cgit/filters/about-formatting.sh";
+            source-filter = "${
+              config.services.cgit."git.migalmoreno.com".package
+            }/lib/cgit/filters/syntax-highlighting.py";
+          };
+          extraConfig = ''
+            readme=:README.md
+            readme=:README.org
+          '';
+        };
+        services.nginx.virtualHosts."_" = {
+          listen = [
+            {
+              port = 8080;
+              addr = "0.0.0.0";
+            }
+          ];
+          root = "/srv/http/migalmoreno.com";
+        };
+        users.users.${config.services.cgit."git.migalmoreno.com".user} = {
+          extraGroups = [ "git" ];
+        };
+        nix.gc = {
+          automatic = true;
+          options = "--delete-older-than 30d";
+        };
         system.stateVersion = "24.05";
       }
     )
@@ -127,6 +184,7 @@ nixpkgs.lib.nixosSystem {
     ../modules/networking
     ../modules/networking/ssh.nix
     ../modules/networking/syncthing.nix
+    ../modules/networking/tailscale.nix
     ../modules/nix.nix
     ../modules/nginx.nix
     ../modules/secrets.nix
