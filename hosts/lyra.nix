6ddd21a8da9ecf1e67a53999cc62cc9fab0b81e5
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-10-14 20:46:39 +0200

    hosts: Use development preset for orion and lyra

--- hosts/lyra.nix
+++ hosts/lyra.nix
@@ -9,6 +9,7 @@ nixpkgs.lib.nixosSystem {
     home-manager.nixosModules.home-manager
     sops-nix.nixosModules.sops
     ordenada.nixosModules.ordenada
+    ../presets/development.nix
     (
       {
         lib,
@@ -88,130 +89,25 @@ nixpkgs.lib.nixosSystem {
           MODE="0666", \
           RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
         '';
-        services.guix.enable = true;
-        sops = {
-          defaultSopsFile = ../secrets.yaml;
-          age.sshKeyPaths = [ "${config.home-manager.users.vega.home.homeDirectory}/.ssh/id_ed25519" ];
-          secrets = {
-            "hosts/lyra/syncthing/key".owner = "vega";
-            "hosts/lyra/syncthing/cert".owner = "vega";
-          };
-        };
-        systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
-        services.syncthing = with config.home-manager.users.vega; {
-          enable = true;
-          user = "vega";
-          key = config.sops.secrets."hosts/lyra/syncthing/key".path;
-          cert = config.sops.secrets."hosts/lyra/syncthing/cert".path;
-          dataDir = "${xdg.dataHome}/syncthing";
-          configDir = "${xdg.configHome}/syncthing";
-          overrideDevices = true;
-          overrideFolders = true;
-          settings = {
-            devices = {
-              auriga.id = "TA7YNZV-D5NBYBY-AFB2UU5-S4YNLRA-DCVV5CB-QP333MS-K7MVD4O-NIWUBQB";
-              orion.id = "JHG7EZC-D52KXLP-AN45CEX-ADKNSST-J4R3XDF-NDI26JH-JIJYZJ5-AEJHFQO";
-              taurus.id = "NLIYTEA-LIL3CAW-N62ZGBA-6DRE2ZM-UNPTBLN-J7JQGBY-RVLYJNT-GD6ZTAG";
-            };
-            folders = {
-              documents = {
-                path = "~/documents";
-                devices = [ "auriga" ];
-              };
-              pictures = {
-                path = "~/pictures";
-                devices = [
-                  "auriga"
-                  "taurus"
-                ];
-              };
-              notes = {
-                path = "~/notes";
-                devices = [
-                  "auriga"
-                  "taurus"
-                ];
-              };
-              videos = {
-                path = "~/videos";
-                devices = [ "auriga" ];
-              };
-              work-projects = {
-                path = "~/src/work";
-                devices = [ "orion" ];
-              };
-              work-notes = {
-                path = "~/notes/work";
-                devices = [ "orion" ];
-              };
-              work-documents = {
-                path = "~/documents/work";
-                devices = [ "orion" ];
-              };
-            };
-          };
-        };
         ordenada = {
-          users = {
-            vega = { };
-          };
+          users.vega = { };
           features = {
             userInfo.username = "vega";
-            keyboard.layout = {
-              name = "us,es";
-              options = [
-                "grp:shifts_toggle"
-                "caps:ctrl_modifier"
-                "altwin:prtsc_rwin"
-              ];
-            };
-            theme.enable = true;
-            theme.polarity = "dark";
-            bash.enable = true;
-            home.enable = true;
-            fontutils.enable = true;
-            clojure.enable = true;
-            pipewire.enable = true;
             git = {
-              enable = true;
               username = "Miguel Ángel Moreno";
               email = "mail@migalmoreno.com";
               signingKey = "5F23F458";
               signCommits = true;
             };
-            gtk = {
-              enable = true;
-              cursorTheme = {
-                name = "Bibata-Modern-${
-                  if config.ordenada.features.theme.polarity == "dark" then "Classic" else "Ice"
-                }";
-                package = pkgs.bibata-cursors;
-              };
-            };
-            docker.enable = true;
-            javascript.enable = true;
-            nix.enable = true;
-            yaml.enable = true;
             gnupg = {
               enable = true;
               sshKeys = [ "D6B4894600BB392AB2AEDE499CBBCF3E0620B7F6" ];
             };
             password-store.enable = true;
-            sway = rec {
-              enable = true;
+            sway = {
               autoStartTty = "/dev/tty1";
               modifier = "Mod4";
-              keybindings = {
-                "${modifier}+j" = "focus left";
-                "${modifier}+k" = "focus right";
-                "${modifier}+Prior" = "exec ${pkgs.pamixer}/bin/pamixer --unmute --increase 5";
-                "${modifier}+Next" = "exec ${pkgs.pamixer}/bin/pamixer --unmute --decrease 5";
-                "${modifier}+x" = "exec ${pkgs.swaylock-effects}/bin/swaylock";
-                "${modifier}+Insert" = "exec pkill -SIGINT -f wf-recorder";
-              };
             };
-            waybar.enable = true;
-            bemenu.enable = true;
             kanshi = {
               enable = true;
               settings = [
@@ -271,24 +167,8 @@ nixpkgs.lib.nixosSystem {
                 }
               ];
             };
-            markdown.enable = true;
             emacs = {
-              enable = true;
-              advancedUser = true;
-              extraPackages = with pkgs.emacsPackages; [
-                wgrep
-                emacs-conflict
-              ];
-              ace-window.enable = true;
-              all-the-icons.enable = true;
-              appearance.enable = true;
-              modus-themes = {
-                enable = true;
-                dark = config.ordenada.features.theme.polarity == "dark";
-              };
-              org.enable = true;
               org-roam = {
-                enable = true;
                 captureTemplates = ''
                   `(("w" "work" plain "%?"
                      :if-new (file+head "work/%<%Y%m%d%H%M%S>-''${slug}.org"
@@ -311,89 +191,72 @@ nixpkgs.lib.nixosSystem {
                                         "#+title: %<%Y-%m-%d>\n")))
                 '';
               };
-              spelling = {
-                enable = true;
-                package = pkgs.aspellWithDicts (
-                  dicts: with dicts; [
-                    en
-                    es
-                  ]
-                );
-                ispellStandardDictionary = "en_US";
-                flyspellHooks = [
-                  "org-mode-hook"
-                  "message-mode-hook"
-                  "bibtex-mode-hook"
-                ];
-              };
-              dired.enable = true;
-              embark.enable = true;
-              corfu = {
-                enable = true;
-                globalModes = [
-                  "(not org-mode)"
-                  "t"
-                ];
-              };
-              consult.enable = true;
-              vertico.enable = true;
-              completion.enable = true;
-              marginalia.enable = true;
-              orderless.enable = true;
-              ebdb.enable = true;
-              eshell.enable = true;
-              help.enable = true;
-              apheleia.enable = true;
-              flymake.enable = true;
-              rainbow-delimiters.enable = true;
-              eglot.enable = true;
-              project.enable = true;
-              shell.enable = true;
-              vterm.enable = true;
-              smartparens = {
-                enable = true;
-                pareditBindings = true;
-                hooks = [
-                  "prog-mode-hook"
-                  "lisp-data-mode-hook"
-                  "minibuffer-inactive-mode-hook"
-                  "comint-mode-hook"
-                  "cider-repl-mode-hook"
-                ];
-                strictHooks = [ ];
-              };
-              tramp.enable = true;
-            };
-            firefox = {
-              enable = true;
-              extensions = with config.nur.repos.rycee.firefox-addons; [
-                ublock-origin
-                multi-account-containers
-                tridactyl
-              ];
-            };
-            direnv.enable = true;
-            compile.enable = true;
-            ssh = with pkgs.secrets.hosts; {
-              enable = true;
-              matchBlocks = {
-                auriga = {
-                  hostname = auriga.address;
-                  user = "root";
-                };
-                capella = {
-                  hostname = auriga.address;
-                  user = "capella";
-                };
-                cygnus = {
-                  hostname = cygnus.address;
-                  user = "root";
-                };
-              };
             };
             networking.enable = true;
             tailscale.enable = true;
-            xdg.enable = true;
+          };
+        };
+        services.guix.enable = true;
+        sops = {
+          defaultSopsFile = ../secrets.yaml;
+          age.sshKeyPaths = [ "${config.home-manager.users.vega.home.homeDirectory}/.ssh/id_ed25519" ];
+          secrets = {
+            "hosts/lyra/syncthing/key".owner = "vega";
+            "hosts/lyra/syncthing/cert".owner = "vega";
+          };
+        };
+        systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
+        services.syncthing = with config.home-manager.users.vega; {
+          enable = true;
+          user = "vega";
+          key = config.sops.secrets."hosts/lyra/syncthing/key".path;
+          cert = config.sops.secrets."hosts/lyra/syncthing/cert".path;
+          dataDir = "${xdg.dataHome}/syncthing";
+          configDir = "${xdg.configHome}/syncthing";
+          overrideDevices = true;
+          overrideFolders = true;
+          settings = {
+            devices = {
+              auriga.id = "FZVCCHW-DF2SVCK-6NFAH3K-A6TTZ6S-D44H3NZ-SHSC7FV-EQL3R2S-J2CR3QZ";
+              orion.id = "JHG7EZC-D52KXLP-AN45CEX-ADKNSST-J4R3XDF-NDI26JH-JIJYZJ5-AEJHFQO";
+              taurus.id = "NLIYTEA-LIL3CAW-N62ZGBA-6DRE2ZM-UNPTBLN-J7JQGBY-RVLYJNT-GD6ZTAG";
+            };
+            folders = {
+              documents = {
+                path = "~/documents";
+                devices = [ "auriga" ];
+              };
+              pictures = {
+                path = "~/pictures";
+                devices = [
+                  "auriga"
+                  "taurus"
+                ];
+              };
+              notes = {
+                path = "~/notes";
+                devices = [
+                  "auriga"
+                  "taurus"
+                ];
+              };
+              videos = {
+                path = "~/videos";
+                devices = [ "auriga" ];
+              };
+              work-projects = {
+                path = "~/src/work";
+                devices = [ "orion" ];
+              };
+              work-notes = {
+                path = "~/notes/work";
+                devices = [ "orion" ];
+              };
+              work-documents = {
+                path = "~/documents/work";
+                devices = [ "orion" ];
+              };
+            };
           };
         };
         virtualisation.oci-containers.containers = {
--- hosts/orion.nix
+++ hosts/orion.nix
@@ -10,6 +10,7 @@ nixpkgs.lib.nixosSystem {
     home-manager.nixosModules.home-manager
     ordenada.nixosModules.ordenada
     sops-nix.nixosModules.sops
+    ../presets/development.nix
     (
       {
         lib,
@@ -18,231 +19,36 @@ nixpkgs.lib.nixosSystem {
         ...
       }:
       {
-        ordenada.users = {
-          saiph = { };
-        };
-        ordenada.features = {
-          userInfo.username = "saiph";
-          keyboard.layout = {
-            name = "us,es";
-            options = [
-              "grp:shifts_toggle"
-              "caps:ctrl_modifier"
-              "altwin:prtsc_rwin"
-            ];
-          };
-          theme.enable = true;
-          theme.polarity = "dark";
-          home.enable = true;
-          bash.enable = true;
-          fontutils.enable = true;
-          clojure.enable = true;
-          pipewire.enable = true;
-          git = with pkgs.secrets.work; {
-            enable = true;
-            username = fullname;
-            email = email;
-            gitLinkRemotes = {
-              "git.${builtins.elemAt domains 0}" = "git-link-gitlab";
-              "git-ext.${builtins.elemAt domains 1}" = "git-link-gitlab";
-            };
-          };
-          gtk = {
-            enable = true;
-            cursorTheme = {
-              name = "Bibata-Modern-${
-                if config.ordenada.features.theme.polarity == "dark" then "Classic" else "Ice"
-              }";
-              package = pkgs.bibata-cursors;
-            };
-          };
-          docker.enable = true;
-          javascript.enable = true;
-          nix.enable = true;
-          yaml.enable = true;
-          gnupg = {
-            enable = true;
-            sshKeys = [ "D6B4894600BB392AB2AEDE499CBBCF3E0620B7F6" ];
-          };
-          password-store.enable = true;
-          sway = rec {
-            enable = true;
-            autoStartTty = "/dev/pts/0";
-            modifier = "Mod2";
-            keybindings = {
-              "${modifier}+j" = "focus left";
-              "${modifier}+k" = "focus right";
-              "${modifier}+Prior" = "exec ${pkgs.pamixer}/bin/pamixer --unmute --increase 5";
-              "${modifier}+Next" = "exec ${pkgs.pamixer}/bin/pamixer --unmute --decrease 5";
-              "${modifier}+x" = "exec ${pkgs.swaylock-effects}/bin/swaylock";
-              "${modifier}+Insert" = "exec pkill -SIGINT -f wf-recorder";
-            };
-          };
-          waybar.enable = true;
-          bemenu.enable = true;
-          markdown.enable = true;
-          emacs = {
-            enable = true;
-            advancedUser = true;
-            ace-window.enable = true;
-            all-the-icons.enable = true;
-            appearance.enable = true;
-            modus-themes = {
-              enable = true;
-              dark = config.ordenada.features.theme.polarity == "dark";
-            };
-            org.enable = true;
-            org-roam = {
-              enable = true;
-              todoIntegration = true;
-            };
-            spelling = {
-              enable = true;
-              package = pkgs.aspellWithDicts (
-                dicts: with dicts; [
-                  en
-                  es
-                ]
-              );
-              ispellStandardDictionary = "en_US";
-              flyspellHooks = [
-                "org-mode-hook"
-                "message-mode-hook"
-                "bibtex-mode-hook"
-              ];
-            };
-            dired.enable = true;
-            embark.enable = true;
-            corfu = {
-              enable = true;
-              globalModes = [
-                "(not org-mode)"
-                "t"
-              ];
-            };
-            consult.enable = true;
-            vertico.enable = true;
-            completion.enable = true;
-            marginalia.enable = true;
-            orderless.enable = true;
-            ebdb.enable = true;
-            eshell.enable = true;
-            help.enable = true;
-            apheleia.enable = true;
-            flymake.enable = true;
-            rainbow-delimiters.enable = true;
-            eglot.enable = true;
-            project.enable = true;
-            shell.enable = true;
-            vterm.enable = true;
-            smartparens = {
-              enable = true;
-              pareditBindings = true;
-              hooks = [
-                "prog-mode-hook"
-                "lisp-data-mode-hook"
-                "minibuffer-inactive-mode-hook"
-                "comint-mode-hook"
-                "cider-repl-mode-hook"
-              ];
-              strictHooks = [ ];
-            };
-            tramp.enable = true;
-          };
-          firefox = {
-            enable = true;
-            extensions = with config.nur.repos.rycee.firefox-addons; [
-              ublock-origin
-              multi-account-containers
-              tridactyl
-            ];
-            extraSearchConfig = {
-              default = "Whoogle";
-              privateDefault = "Whoogle";
-              order = [
-                "Whoogle"
-                "Google"
-              ];
-              engines = {
-                "Google".metaData.alias = "@g";
-                "Whoogle" = {
-                  iconUpdateURL = "http://localhost:5000/favicon.ico";
-                  updateInterval = 24 * 60 * 60 * 1000;
-                  definedAliases = [ "@w" ];
-                  urls = [
-                    {
-                      template = "http://localhost:5000/search";
-                      params = [
-                        {
-                          name = "q";
-                          value = "{searchTerms}";
-                        }
-                      ];
-                    }
-                  ];
-                };
-              };
-            };
-          };
-          direnv.enable = true;
-          compile.enable = true;
-          ssh = with pkgs.secrets.hosts; {
-            enable = true;
-            daemon = false;
-            matchBlocks = {
-              auriga = {
-                hostname = auriga.address;
-                user = "root";
-              };
-              capella = {
-                hostname = auriga.address;
-                user = "capella";
-              };
-              cygnus = {
-                hostname = cygnus.address;
-                user = "root";
-              };
-            };
-          };
-          xdg = {
-            enable = true;
-            userDirs = with config.ordenada.features.userInfo; {
-              enable = true;
-              createDirectories = true;
-              desktop = null;
-              documents = "${homeDirectory}/documents";
-              download = "${homeDirectory}/downloads";
-              music = "${homeDirectory}/music";
-              pictures = "${homeDirectory}/pictures";
-              publicShare = "${homeDirectory}/public";
-              templates = null;
-              videos = "${homeDirectory}/videos";
-            };
-          };
-        };
+        time.timeZone = "Europe/Madrid";
+        hardware.graphics.enable = true;
+        nixpkgs.overlays = overlays;
+        networking.hostName = "orion";
+        networking.firewall.enable = false;
         environment.systemPackages = with pkgs; [
           libreoffice
           git-agecrypt
           pavucontrol
           alacritty
         ];
-        networking.hostName = "orion";
-        networking.firewall.enable = false;
-        security.pki.certificateFiles = [ ../secrets/ca-bundle.crt ];
-        systemd.services.wsl-vpnkit = {
-          enable = true;
-          description = "wsl-vpnkit";
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          serviceConfig = {
-            ExecStart = "${pkgs.wsl-vpnkit}/bin/wsl-vpnkit";
-            Restart = "on-failure";
-            KillMode = "mixed";
+        ordenada = {
+          users.saiph = { };
+          features = {
+            userInfo.username = "saiph";
+            git = with pkgs.secrets.work; {
+              username = fullname;
+              email = email;
+              gitLinkRemotes = {
+                "git.${builtins.elemAt domains 0}" = "git-link-gitlab";
+                "git-ext.${builtins.elemAt domains 1}" = "git-link-gitlab";
+              };
+            };
+            sway = {
+              autoStartTty = "/dev/pts/0";
+              modifier = "Mod2";
+            };
           };
         };
-        users.extraGroups.docker.members = [ "saiph" ];
-        time.timeZone = "Europe/Madrid";
-        hardware.graphics.enable = true;
+        security.pki.certificateFiles = [ ../secrets/ca-bundle.crt ];
         virtualisation.docker = {
           daemon.settings = {
             bip = "10.2.20.10/16";
@@ -268,15 +74,6 @@ nixpkgs.lib.nixosSystem {
               WHOOGLE_RESULTS_PER_PAGE = "50";
               WHOOGLE_CONFIG_SEARCH_LANGUAGE = "lang_en";
             };
-          };
-        };
-        wsl = {
-          enable = true;
-          defaultUser = "saiph";
-          wslConf = {
-            network.generateResolvConf = true;
-            network.generateHosts = true;
-            user.default = "saiph";
           };
         };
         sops = {
@@ -317,11 +114,30 @@ nixpkgs.lib.nixosSystem {
             };
           };
         };
-        nixpkgs.overlays = overlays;
+        wsl = {
+          enable = true;
+          defaultUser = "saiph";
+          wslConf = {
+            network.generateResolvConf = true;
+            network.generateHosts = true;
+            user.default = "saiph";
+          };
+        };
+        systemd.services.wsl-vpnkit = {
+          enable = true;
+          description = "wsl-vpnkit";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.wsl-vpnkit}/bin/wsl-vpnkit";
+            Restart = "on-failure";
+            KillMode = "mixed";
+          };
+        };
         home-manager.users.saiph = {
           systemd.user.services.wsl-clipboard = {
             Unit = {
-              Description = "WSL clibpoard sharing";
+              Description = "WSL clipboard sharing";
               After = [ "sway-session.target" ];
             };
             Install = {
