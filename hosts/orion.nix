f9e77d4304d2ed2f6fd9388b63a8bac064148d0d
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-16 09:54:38 +0100

    orion: Add binfmt emulation

--- hosts/orion.nix
+++ hosts/orion.nix
@@ -24,6 +24,7 @@ nixpkgs.lib.nixosSystem {
         nixpkgs.overlays = overlays;
         networking.hostName = "orion";
         networking.firewall.enable = false;
+        boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
         environment.systemPackages = with pkgs; [
           libreoffice
           git-agecrypt
