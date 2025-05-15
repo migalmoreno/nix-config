7491a935e08ad0ec654d9dfc6cf84526f6f2a709
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-05-15 21:42:48 +0200

    feat(cygnus): add tubo-db setup

--- hosts/cygnus.nix
+++ hosts/cygnus.nix
@@ -77,18 +77,38 @@ nixpkgs.lib.nixosSystem {
             environment = {
               BACKEND_URL = "https://api.tubo.media";
             };
+            extraOptions = [ "--pull=newer" ];
           };
           tubo-backend = {
             image = "migalmoreno/tubo-backend";
             ports = [ "3000:3000" ];
-            extraOptions = [ "--network=container:gluetun" ];
-            dependsOn = [ "gluetun" ];
+            extraOptions = [
+              "--network=container:gluetun"
+              "--pull=newer"
+            ];
+            dependsOn = [
+              "gluetun"
+              "tubo-db"
+            ];
+            environmentFiles = [ config.sops.templates."tubo-backend.env".path ];
           };
           tubo-bg-helper = {
             image = "migalmoreno/tubo-bg-helper";
             ports = [ "3005:3005" ];
-            extraOptions = [ "--network=container:gluetun" ];
+            extraOptions = [
+              "--network=container:gluetun"
+              "--pull=newer"
+            ];
             dependsOn = [ "gluetun" ];
+          };
+          tubo-db = {
+            image = "postgres:16-alpine";
+            extraOptions = [
+              "--network=container:gluetun"
+            ];
+            dependsOn = [ "gluetun" ];
+            volumes = [ "/var/local/db/tubo:/var/lib/postgresql/data" ];
+            environmentFiles = [ config.sops.templates."tubo-db.env".path ];
           };
           gluetun = {
             image = "qmcgaw/gluetun";
@@ -108,8 +128,20 @@ nixpkgs.lib.nixosSystem {
           age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
           secrets = {
             "hosts/cygnus/gluetun/config" = { };
+            "hosts/cygnus/tubo/db/password" = { };
           };
           templates."gluetun.env".content = config.sops.placeholder."hosts/cygnus/gluetun/config";
+          templates."tubo-backend.env".content = ''
+            DB_HOST=tubo-db
+            DB_NAME=tubo
+            DB_USER=tubo
+            DB_PASSWORD=${config.sops.placeholder."hosts/cygnus/tubo/db/password"}
+          '';
+          templates."tubo-db.env".content = ''
+            POSTGRES_DB=tubo
+            POSTGRES_USER=tubo
+            POSTGRES_PASSWORD=${config.sops.placeholder."hosts/cygnus/tubo/db/password"}
+          '';
         };
         networking.firewall.allowedTCPPorts = [
           80
