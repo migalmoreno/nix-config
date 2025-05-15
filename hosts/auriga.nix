13eb3197d8e80bc57a644628350d36aa0d739c65
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-05-15 21:24:52 +0200

    feat(auriga): add tubo-db setup

--- hosts/auriga.nix
+++ hosts/auriga.nix
@@ -92,6 +92,7 @@ nixpkgs.lib.nixosSystem {
             docker.enable = true;
           };
         };
+        services.resolved.enable = true;
         nixarr = {
           enable = true;
           jellyfin.enable = true;
@@ -130,7 +131,19 @@ nixpkgs.lib.nixosSystem {
             };
             "hosts/auriga/syncthing/key" = { };
             "hosts/auriga/syncthing/cert" = { };
+            "hosts/auriga/tubo/db/password" = { };
           };
+          templates."tubo-backend.env".content = ''
+            DB_HOST=tubo-db
+            DB_NAME=tubo
+            DB_USER=tubo
+            DB_PASSWORD=${config.sops.placeholder."hosts/auriga/tubo/db/password"}
+          '';
+          templates."tubo-db.env".content = ''
+            POSTGRES_DB=tubo
+            POSTGRES_USER=tubo
+            POSTGRES_PASSWORD=${config.sops.placeholder."hosts/auriga/tubo/db/password"}
+          '';
         };
         systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
         services.syncthing = {
@@ -448,15 +461,38 @@ nixpkgs.lib.nixosSystem {
             environment = {
               BACKEND_URL = "http://auriga:3000";
             };
+            extraOptions = [ "--pull=newer" ];
           };
           tubo-backend = {
             image = "migalmoreno/tubo-backend";
             ports = [ "3000:3000" ];
+            extraOptions = [ "--pull=newer" ];
+            dependsOn = [ "tubo-db" ];
+            environmentFiles = [ config.sops.templates."tubo-backend.env".path ];
           };
           tubo-bg-helper = {
             image = "migalmoreno/tubo-bg-helper";
             ports = [ "3005:3005" ];
+            extraOptions = [ "--pull=newer" ];
           };
+          tubo-db = {
+            image = "postgres:16-alpine";
+            ports = [ "5432:5432" ];
+            volumes = [ "/var/local/db/tubo:/var/lib/postgresql/data" ];
+            environmentFiles = [ config.sops.templates."tubo-db.env".path ];
+          };
+        };
+        systemd.services.pgweb = {
+          enable = false;
+          description = "pgweb";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.pgweb}/bin/pgweb";
+            Restart = "on-failure";
+            KillMode = "mixed";
+          };
+          environment.PGWEB_DATABASE_URL = "postgres://tubo:tubo@localhost:5432/tubo";
         };
         nixpkgs.config.permittedInsecurePackages = [
           "aspnetcore-runtime-wrapped-6.0.36"
