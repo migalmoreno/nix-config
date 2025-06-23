bf2850cfb2f02281b4d48680bf43c79281a9ae47
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-06-23 16:59:38 +0200

    feat(auriga): add searxng

--- hosts/auriga.nix
+++ hosts/auriga.nix
@@ -227,6 +227,46 @@ inputs.nixpkgs.lib.nixosSystem {
           ];
           httpOrigins = [ "*" ];
         };
+        services.searx = {
+          enable = true;
+          redisCreateLocally = true;
+          settings = {
+            general = {
+              debug = false;
+            };
+            server = {
+              port = 8888;
+              bind_address = "0.0.0.0";
+            };
+            search = {
+              favicon_resolver = "google";
+              autocomplete = "google";
+              autocomplete_min = 2;
+              method = "GET";
+            };
+            ui = {
+              infinite_scroll = true;
+              default_theme = "simple";
+            };
+            categories_as_tabs = {
+              general = { };
+              images = { };
+              videos = { };
+              news = { };
+            };
+            engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
+              "duckduckgo".disabled = true;
+              "qwant".disabled = true;
+              "startpage".disabled = false;
+              "piped".disabled = true;
+              "vimeo".disabled = true;
+              "dailymotion".disabled = true;
+              "library genesis".disabled = false;
+              "reddit".disabled = false;
+            };
+          };
+          environmentFile = config.sops.templates."searxng.env".path;
+        };
         systemd.services.homepage-dashboard.serviceConfig.Group = "podman";
         services.homepage-dashboard = {
           enable = true;
