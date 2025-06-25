acdc34f82187a9b954ca8061cb3d45e70a739d1f
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-06-25 13:35:24 +0200

    feat(cygnus): add GoAccess setup

--- hosts/cygnus.nix
+++ hosts/cygnus.nix
@@ -264,6 +264,15 @@ inputs.nixpkgs.lib.nixosSystem {
             forceSSL = true;
           };
         };
+        services.goaccess = {
+          enable = true;
+          logFilePath = "/var/log/nginx/access.log";
+          logFileFormat = "COMBINED";
+          serverHost = "cygnus";
+          serverPort = 8081;
+          enableNginx = true;
+        };
+        users.users.${config.services.goaccess.user}.extraGroups = [ config.services.nginx.group ];
         services.nginx = {
           enable = true;
           enableReload = true;
@@ -274,6 +283,14 @@ inputs.nixpkgs.lib.nixosSystem {
           appendHttpConfig = ''
             limit_req_zone $binary_remote_addr zone=ip:20m rate=10r/s;
             limit_req_status 429;
+          '';
+          commonHttpConfig = ''
+            log_format vcombined '$host:$server_port '
+                                 '$remote_addr $remote_user [$time_local] '
+                                 '"$request" $status $body_bytes_sent '
+                                 '"$http_referer" '
+                                 '"$http_user_agent"';
+            access_log /var/log/nginx/access.log vcombined;
           '';
           virtualHosts =
             let
