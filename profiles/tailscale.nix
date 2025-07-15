11269f9d10b948da8e36a64dc1b12c0950edf7e6
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-07-15 02:17:44 +0200

    feat(tailscale): add profile options

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,11 +1,25 @@
-{ config, pkgs, ... }:
-
 {
-  environment.systemPackages = [ pkgs.tailscale ];
-  services.tailscale.enable = true;
-  networking.firewall = {
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-    allowedTCPPorts = [ 22 ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.profiles.tailscale;
+in
+{
+  options.profiles.tailscale = {
+    enable = mkEnableOption "the Tailscale profile";
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.tailscale ];
+    services.tailscale.enable = true;
+    networking.firewall = {
+      trustedInterfaces = [ "tailscale0" ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+      allowedTCPPorts = [ 22 ];
+    };
   };
 }
