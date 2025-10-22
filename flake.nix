68ebc5b35c2e46900608a3016d5910ba04b6cc01
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-10-23 00:29:42 +0200

    feat: add flake-parts flake setup

--- flake.nix
+++ flake.nix
@@ -28,59 +28,69 @@
     };
     systems.url = "github:nix-systems/default";
     filestash-nix.url = "github:dermetfan/filestash.nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
   outputs =
-    inputs@{ nixpkgs, systems, ... }:
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      systems,
+      ...
+    }:
     let
       inherit (nixpkgs) lib;
-      eachSystem = f: lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
       readDirFilenames =
         dir:
         (builtins.filter (file: lib.hasSuffix ".nix" file.name || file.value == "directory") (
           lib.mapAttrsToList (name: value: { inherit name value; }) (builtins.readDir dir)
         ));
     in
-    rec {
-      images = {
-        taurus = nixosConfigurations.taurus.config.mobile.outputs.android.android-fastboot-images;
-      };
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            git-agecrypt
-            ssh-to-age
-            home-manager
-            sops
-            nixos-rebuild
-            nixos-install-tools
-          ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      flake = rec {
+        images = {
+          taurus = nixosConfigurations.taurus.config.mobile.outputs.android.android-fastboot-images;
         };
-      });
-      nixosConfigurations =
-        let
-          secrets = builtins.fromJSON (builtins.readFile ./secrets/secrets.json);
-          overlays = [
-            (final: prev: { inherit secrets; })
-          ];
-          pkgs = import nixpkgs { inherit overlays; };
-        in
-        builtins.listToAttrs (
-          map (file: rec {
-            name = nixpkgs.lib.removeSuffix ".nix" file.name;
-            value = import (./hosts + "/${name}") { inherit inputs pkgs overlays; };
-          }) (readDirFilenames ./hosts)
-        );
-      homeConfigurations = builtins.listToAttrs (
-        map
-          (user: {
-            name = user;
-            value = user.home;
-          })
-          (
-            lib.concatMap (host: (builtins.attrNames nixosConfigurations.${host}.config.home-manager.users)) (
-              builtins.attrNames nixosConfigurations
+        nixosConfigurations =
+          let
+            secrets = builtins.fromJSON (builtins.readFile ./secrets/secrets.json);
+            overlays = [
+              (final: prev: { inherit secrets; })
+            ];
+            pkgs = import nixpkgs { inherit overlays; };
+          in
+          builtins.listToAttrs (
+            map (file: rec {
+              name = nixpkgs.lib.removeSuffix ".nix" file.name;
+              value = import (./hosts + "/${name}") { inherit inputs pkgs overlays; };
+            }) (readDirFilenames ./hosts)
+          );
+        homeConfigurations = builtins.listToAttrs (
+          map
+            (user: {
+              name = user;
+              value = user.home;
+            })
+            (
+              lib.concatMap (host: (builtins.attrNames nixosConfigurations.${host}.config.home-manager.users)) (
+                builtins.attrNames nixosConfigurations
+              )
             )
-          )
-      );
+        );
+      };
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              git-agecrypt
+              ssh-to-age
+              home-manager
+              sops
+              nixos-rebuild
+              nixos-install-tools
+            ];
+          };
+        };
     };
 }
